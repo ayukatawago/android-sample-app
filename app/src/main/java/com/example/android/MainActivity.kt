386d2c8cdc920312957279687894467179189b3e
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2019-02-22 05:33:13 +0900

    use TestFragment in MainActivity

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.ui.TestFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, TestFragment())
+            .commit()
     }
 }
