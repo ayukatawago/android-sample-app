77eb1ef05f5c4f0908b48b7713813524464c34d0
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2019-03-18 14:11:26 +0900

    support DrawerLayout

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.nav_host)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        setSupportActionBar(toolbar)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer)
+        NavigationUI.setupWithNavController(nav_view, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawer) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
