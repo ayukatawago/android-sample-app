e43be35a893bbe40c8dcdae72e5183fe9720d45e
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2019-03-27 06:21:31 +0900

    app: reformat code

--- app/src/main/java/com/example/android/ui/HomeFragment.kt
+++ app/src/main/java/com/example/android/ui/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.android.ui
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.R
 
 class HomeFragment : Fragment() {
