c5a58d42c4adc5471fae9ec57cc440cbc45c7ccd
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2019-04-16 06:29:34 +0900

    listview: use coroutines

--- listview/src/main/java/com/example/listview/ui/ListViewFragment.kt
+++ listview/src/main/java/com/example/listview/ui/ListViewFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.listview.R
 import com.example.listview.data.AndroidDataRepositoryImpl
 import kotlinx.android.synthetic.main.list_view_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 class ListViewFragment : Fragment() {
@@ -32,10 +36,17 @@ class ListViewFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        listview?.apply {
-            val data = viewModel.data
-            val adapter = AndroidArrayAdapter(context, R.layout.list_item_layout, data)
-            setAdapter(adapter)
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = viewModel.data.await()
+
+            data.observe(this@ListViewFragment, Observer {
+                if (it == null) return@Observer
+
+                listview?.apply {
+                    val adapter = AndroidArrayAdapter(context, R.layout.list_item_layout, it)
+                    setAdapter(adapter)
+                }
+            })
         }
     }
 
