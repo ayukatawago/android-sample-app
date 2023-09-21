38148c684405b73d7827ad84d48589ef13f60e66
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2023-09-21 16:43:41 +0900

    Add header toolbar to BrowserActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
--- app/src/main/java/com/example/android/browser/BrowserActivity.kt
+++ app/src/main/java/com/example/android/browser/BrowserActivity.kt
@@ -3,7 +3,10 @@ package com.example.android.browser
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.R
 import com.example.android.databinding.BrowserActivityLayoutBinding
 
 class BrowserActivity : AppCompatActivity() {
@@ -12,6 +15,21 @@ class BrowserActivity : AppCompatActivity() {
 
         val viewBinding = BrowserActivityLayoutBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        setSupportActionBar(viewBinding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.header_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.closeButton -> finish()
+            else -> Unit
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
