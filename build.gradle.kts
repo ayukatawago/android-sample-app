c1c79f26f421acef36f10bf3fbeeb79f5cd9da5b
Takuya Ogawa <46732412+ayukatawago@users.noreply.github.com>
2023-09-21 14:53:14 +0900

    Add ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ktlint)
+}
+
+subprojects {
+    afterEvaluate {
+        apply(plugin = libs.plugins.ktlint.get().pluginId)
+    }
 }
